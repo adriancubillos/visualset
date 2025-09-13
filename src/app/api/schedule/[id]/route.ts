e57d118c54e9e73ba6c8b795e1d2102dfee9d299
Adrian Cubillos <adriancubillos@gmail.com>
2025-09-12 22:31:10 -0400

    find scheduled taks by id

---
+++ src/app/api/schedule/[id]/route.ts
@@ -0,0 +1,28 @@
+import { NextResponse } from 'next/server';
+import { PrismaClient } from '@prisma/client';
+
+const prisma = new PrismaClient();
+
+// GET /api/schedule/:id
+export async function GET(_: Request, { params }: { params: { id: string } }) {
+  try {
+    const task = await prisma.task.findUnique({
+      where: { id: params.id },
+      include: {
+        machine: true,
+        operator: true,
+      },
+    });
+
+    if (!task) {
+      return NextResponse.json({ error: 'Task not found' }, { status: 404 });
+    }
+
+    return NextResponse.json(task);
+    //BUG fix
+    // eslint-disable-next-line @typescript-eslint/no-explicit-any
+  } catch (error: any) {
+    console.error(error);
+    return NextResponse.json({ error: error.message }, { status: 500 });
+  }
+}
